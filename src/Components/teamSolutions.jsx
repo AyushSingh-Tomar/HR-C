afb2366a620342d7c30b77c4ce4edf229a003b90
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-03-27 19:55:51 +0530

    removed box shadow

--- src/Components/teamSolutions.jsx
+++ src/Components/teamSolutions.jsx
@@ -24,7 +24,7 @@ const TeamSolutions = () => {
           <img
             src={image95}
             alt="Team Solutions"
-            className="w-full max-w-[90%] sm:max-w-[80%] md:max-w-3xl xl:max-w-5xl shadow-xl rounded-lg relative left-[5vw]"
+            className="w-full max-w-[90%] sm:max-w-[80%] md:max-w-3xl xl:max-w-5xl  rounded-lg relative left-[5vw]"
           />
         </div>
 
@@ -32,7 +32,8 @@ const TeamSolutions = () => {
           className="text-gray-800 max-w-4xl mx-auto mt-4 sm:mt-6 text-lg sm:text-xl lg:text-2xl leading-relaxed"
           style={{ fontFamily: 'Poppins, sans-serif', fontSize: '1.2rem', textAlign: 'justify' }}
         >
-          Team Solutions is a quick, time-bound and proven decision making process with collective learning and execution effectiveness,
+          Team Solutions<span>{'\u00AE'}</span><span>{'\u00A0'}</span>
+          is a quick, time-bound and proven decision making process with collective learning and execution effectiveness,
 aided by experts and experienced coaches. The process benefits individuals, teams and organizations by building team synergies,
 while solving problems and developing decision making capabilities alongside.
         </p>
